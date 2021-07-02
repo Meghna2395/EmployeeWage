fc0813d6085467cbc522d84f638c794a0b38e666
Meghna2395 <meghnaborkar23@gmail.com>
2021-07-02 13:06:52 +0530

    EmployeeWageUC1

---
+++ EmployeeWage.java
@@ -0,0 +1,16 @@
+public class EmployeeWage{
+
+     public static void main (String args[]){
+      System.out.println("Welcome");
+      int  isPresent = 1;
+       double check = Math.floor(Math.random()*10)%2;
+       if (check == isPresent ){
+        System.out.println("Employee is Prseent");
+       }
+      else 
+            System.out.println("Employee is Absent"); 
+
+      }
+
+          }
+
