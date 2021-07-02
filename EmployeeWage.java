5519cdfe030064c95967cbaf399e56284bd0a722
Meghna2395 <meghnaborkar23@gmail.com>
2021-07-02 14:02:56 +0530

    EmployeeWageUC4

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,22 +1,23 @@
 public class EmployeeWage{
 
-     public static void main (String args[]){
-      int Full_Time = 1;
-      int Part_Time = 2;
-      int Rate_Per_Hour =20;
-  
-           int empHrs=0;
-           int empWage =0;
-            double empCheck = Math.floor(Math.random() * 10) %3;
-         if (empCheck == Part_Time)
-       empHrs = 4;
-      else if (empCheck ==  Full_Time)
-           empHrs = 8;
-       else 
-            empHrs = 0;
-    empWage = empHrs * Rate_Per_Hour;
-    System.out.println("Emp Wage:"+ empWage);
-
-
-        }
+     public static final int Part_Time = 1;
+            public static final int Full_Time = 2;
+            public static final int Rate_Per_hour = 20;
+             public static void main(String args[]){
+ int empHrs = 0;
+  int empWage = 0;
+int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+switch (empCheck) {
+case Part_Time :
+empHrs = 4;
+break;
+case  Full_Time:
+empHrs = 8;
+break;
+default:
+empHrs = 0;
+}
+empWage = empHrs *  Rate_Per_hour;
+System.out.println("Emp Wage: " + empWage);
+}
 } 
