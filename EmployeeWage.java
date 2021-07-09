ffa51b480ed21dcfb1b717487eb070fd0e3e35ab
Chargeahead <meghnaborkar23@gmail.com>
2021-07-09 23:37:07 +0530

    EmployeeWageUC10

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,52 +1,27 @@
 public class EmployeeWage {
-    public static final int parttime= 1;
-    public static final int fulltime=2;
-
-    private final String company;
-    private final int empRatePerHour;
-    private  final int numOfWorkingDays;
-    private int totalEmpWage;
+      
+    public final String company;
+    public final int empRatePerHour;
+    public  final int numOfWorkingDays;
+    public final int maxHoursPerMonth;
+    public int totalEmpWage;
 
 public EmployeeWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth){
 
      this.company = company;
+     this.empRatePerHour = empRatePerHour;
      this.empRatePerHour = numOfWorkingDays;
      this.maxHoursPerMonth = maxHoursPerMonth;
 }
 
-public void computeEmpWage(){
-  int empHrs = 0, totalEmpHrs = 0 , totalWorkingDays =0;
-while(totalEmpHrs <= maxHoursPerMonth && 
-                totalWorkingDays < numOfWorkingDays){
-  totalWorkingDays++;
-int  empCheck = (int) Math.floor(Math.random() * 10)% 3;
-switch (empCheck){
-case parttime:
- empHrs = 4;
-break;
-case fulltime:
- empHrs = 8;
-default:
-   empHrs = 0;
-}
-
-totalEmpHrs += empHrs;
-System.out.println("Day#:" + totalWorkingDays + "Emp Hr: " + empHrs);
-}
-totalEmpWage = totalEmpHrs * empRatePerHour;
+public void setTotalEmpWage(int totalEmpWage){
+   this.totalEmpWage = totalEmpWage;
 }
 
 public String toString(){
-   return "Total Emp Wage for Company: "+company+" is: "+ totalEmpWage;
-}
-
-public static void main (String[] args){
- EmployeeWage atos = new EmployeeWage("Atos",20,5,40);
- EmployeeWage wipro = new EmployeeWage("wipro",10,4,20);
-System.out.println(atos);
-wipro.computeEmpWage();
-System.out.println("wipro");
+   return "Total Emp Wage for Company: " +company+" is: "+totalEmpWage;
   }
 }
+
 
 
